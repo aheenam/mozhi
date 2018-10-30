416d5cc42833a085a281c23e6dd7b3f18d9303c1
Rathes Sachchithananthan <sachchi@rathes.de>
2018-10-31 00:22:10 +0100

    🔥 Remove template name from template renderer --- It is now the job of a document to have a valid template name value

--- src/TemplateRenderer.php
+++ src/TemplateRenderer.php
@@ -44,28 +44,16 @@ class TemplateRenderer
     public function render($data = [])
     {
         $currentTheme = self::getCurrentTheme();
-        $template = $this->getTemplate();
+        $template = $this->page->getTemplateName();
 
         if (! view()->exists("theme::$currentTheme.$template")) {
             throw new TemplateNotFoundException("Template [$template] was not found in theme [$currentTheme]");
         }
 
         return view("theme::$currentTheme.$template", collect([
-            'meta'    => $this->page->meta(),
-            'content' => $this->page->getParsedContent(),
+            'meta'    => $this->page->getMetaData(),
+            'content' => $this->page->getHtmlContent(),
         ])->concat(collect($data)))->render();
-    }
-
-    /**
-     * @return array|\Illuminate\Config\Repository|mixed|null|string
-     */
-    public function getTemplate()
-    {
-        if ($this->template === null) {
-            $this->template = $this->resolveTemplate();
-        }
-
-        return $this->template;
     }
 
     /**
@@ -74,20 +62,5 @@ class TemplateRenderer
     public static function getCurrentTheme()
     {
         return config('mozhi.theme');
-    }
-
-    /**
-     * returns the template of the page
-     * defaults to default_template of config if none set.
-     *
-     * @return array|\Illuminate\Config\Repository|mixed|null
-     */
-    protected function resolveTemplate()
-    {
-        if ($this->page->meta('template') === null) {
-            return config('mozhi.default_template');
-        }
-
-        return $this->page->meta('template');
     }
 }
